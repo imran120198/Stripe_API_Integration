15b1ba96b0877abe8140166cc3e9c86cf39f1a82
Imran Khan <99397606+imran120198@users.noreply.github.com>
2024-05-17 09:48:00 +0530

    frontend

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,25 +1,9 @@
-import React from "react";
-import Navbar from "../Components/Navbar";
-import { Routes, Route } from "react-router-dom";
-import Home from "../Pages/Home";
-import Cart from "../Pages/Cart";
-import Success from "../Pages/Success";
-import Failure from "../Pages/Failure";
-import Checkout from "../Pages/Checkout";
+import React from 'react'
 
 const AllRoutes = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/failure" element={<Failure />} />
-      </Routes>
-    </div>
-  );
-};
+    <div>AllRoutes</div>
+  )
+}
 
-export default AllRoutes;
+export default AllRoutes
